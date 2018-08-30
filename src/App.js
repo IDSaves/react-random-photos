ded3af24e4842c6877c5a95492cab20a02cea276
DavydovProjects <Ivan2002dav@gmail.com>
2018-08-30 20:57:50 +0300

    Download button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   state = {
@@ -9,6 +9,7 @@ class App extends Component {
   handleClick = () => {
     this.setState({status: false, img_refresh_key: Math.random()})
   }
+
   onLoadImage = () => {
     this.setState({status: true})
   }
@@ -20,21 +21,24 @@ class App extends Component {
 
         <div className="container">
           <div className="row p-3">
-            <div className={status ? "main col-lg-6 offset-lg-3 mx-auto p-3" : "main hide col-lg-6 offset-lg-3 mx-auto p-3"}>
-              <div className={status ? "button text-white pt-2 pb-2 text-center mx-auto" : 'button active text-white pt-2 pb-2 text-center mx-auto'} onClick={status ? this.handleClick : ''}>
+
+            <div className={status ? "main col-lg-4 offset-lg-4 mx-auto pt-3" : "main hide col-lg-4 offset-lg-4 mx-auto pt-3"}>
+              <div className={status ? "button text-white pt-2 pb-2 text-center mx-auto mb-3" : 'button active text-white pt-2 pb-2 text-center mx-auto mb-3'} onClick={status ? this.handleClick : null}>
                 <h3 className="m-0">{status ? 'Get image' : 'Loading...'}</h3>
               </div>
-              <center>
+              <center className="image-block">
                 <div className={status ? "image" : "image hide"}>
-                    <img className="img-fluid" src={'https://picsum.photos/g/450/350/?random' + img_refresh_key} onLoad={this.onLoadImage}/>
+                    <img className="img-fluid" src={'https://picsum.photos/g/450/350/?random' + img_refresh_key} onLoad={this.onLoadImage} ref="image"/>
                 </div>
+                <a className={status ? "download mb-0" : "download hide mb-0"} href={this.refs.image ? this.refs.image.src : ''} target="_blank">Download</a>
               </center>
             </div>
+
           </div>
         </div>
 
       </div>
-    );
+    )
   }
 }
 
