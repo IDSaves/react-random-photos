3594012d03ad7192e703bda391550abd34534393
DavydovProjects <Ivan2002dav@gmail.com>
2018-08-30 19:55:37 +0300

    Better design

--- src/App.js
+++ src/App.js
@@ -17,16 +17,22 @@ class App extends Component {
     const { status, img_refresh_key } = this.state
     return (
       <div className="App mt-5">
+
         <div className="container">
-          <div className={status ? "button text-white pt-2 pb-2 text-center mx-auto" : 'button active text-white pt-2 pb-2 text-center mx-auto'} onClick={status ? this.handleClick : ''}>
-            <h3 className="m-0">{status ? 'Get image' : 'Loading...'}</h3>
-          </div>
-          <div className={status ? "image mt-5" : "image hide mt-5"}>
-            <center>
-              <img className="img-fluid" src={'https://picsum.photos/g/450/350/?random' + img_refresh_key} onLoad={this.onLoadImage}/>
-            </center>
+          <div className="row p-3">
+            <div className={status ? "main col-lg-6 offset-lg-3 mx-auto p-3" : "main hide col-lg-6 offset-lg-3 mx-auto p-3"}>
+              <div className={status ? "button text-white pt-2 pb-2 text-center mx-auto" : 'button active text-white pt-2 pb-2 text-center mx-auto'} onClick={status ? this.handleClick : ''}>
+                <h3 className="m-0">{status ? 'Get image' : 'Loading...'}</h3>
+              </div>
+              <center>
+                <div className={status ? "image" : "image hide"}>
+                    <img className="img-fluid" src={'https://picsum.photos/g/450/350/?random' + img_refresh_key} onLoad={this.onLoadImage}/>
+                </div>
+              </center>
+            </div>
           </div>
         </div>
+
       </div>
     );
   }
